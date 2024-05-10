81f38644fe96f7d1c83286bc0ecae5fe0002af5e
Nikhil K <nikhil.karanje90@gmail.com>
2024-05-10 12:14:00 +0530

    Add Switch Case basic program

--- src/exe_patterns_20240510/Pattern01.java
+++ src/exe_patterns_20240510/Pattern01.java
@@ -1,2 +1,22 @@
-package exe_patterns_20240510;public class Pattern01 {
+package exe_patterns_20240510;
+
+public class Pattern01 {
+    public static void main(String[] args) {
+
+        //Print below * pattern using nested loops
+        // *****
+        // *****
+        // *****
+
+
+        for (int i = 1 ; i <=4; i++){
+
+            for (int j = 1; j<= 5; j++ ) {
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
 }
