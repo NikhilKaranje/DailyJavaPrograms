b81188ddcbac333a91fed10fd90571fec4e12758
Nikhil K <nikhil.karanje90@gmail.com>
2024-05-10 12:14:00 +0530

    patterns complex program

---
+++ src/exe_patterns_20240510/Patterns02.java
@@ -0,0 +1,29 @@
+package exe_patterns_20240510;
+
+public class Patterns02 {
+    public static void main(String[] args) {
+
+        //Patterns complex program
+        // *******
+        // *     *
+        // *     *
+        // *******
+
+        //Declare rows and columns value
+        int n = 4;
+        int m = 5;
+
+        //Outer loop
+        for(int i = 1; i<= n; i++) {
+            for (int j= 1; j<=m; j++){
+
+                if (i == 1 || j ==1 || i == n || j == m) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
